c38c8968d90862ec35f2362a6e90dd070d444afc
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-07-11 12:48:43 -0300

    [:heavy_plus_sign:] | add: dep - @nestjs/axios

--- hexagonal_architeture/src/main.ts
+++ hexagonal_architeture/src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const docConfig = new DocumentBuilder()
+    .setTitle('Hexagonal Architecture')
+    .setDescription(
+      'API That apply Hexagonal Architecture for integrate with a CRM',
+    )
+    .setVersion('1.0')
+    .addTag('hexagonal')
+    .setContact(
+      'Hexagonal Architeture - Repository',
+      'https://github.com/Samuel-Ricardo/nestjs_hexagonal-architeture',
+      'samuelricardoofficial@gmail.com',
+    )
+    .setLicense(
+      'MIT',
+      'https://github.com/Samuel-Ricardo/nestjs_hexagonal-architeture/blob/main/LICENSE',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, docConfig);
+  SwaggerModule.setup('api', app, document);
+
+  console.log('[SETUP] DOCS - https://localhost:3000/api');
+  console.log('[SETUP] DOCS FILE - https://localhost:3000/api-json');
+
   await app.listen(3000);
 }
 bootstrap();
