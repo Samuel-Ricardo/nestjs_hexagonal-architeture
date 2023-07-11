921427d4d90ef9fdfb12b4497e0ec49fd6100227
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-07-11 17:49:21 -0300

    [:white_check_mark:] | update: test - service (lists)

--- hexagonal_architeture/src/lists/lists.service.spec.ts
+++ hexagonal_architeture/src/lists/lists.service.spec.ts
@@ -1,18 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ListsService } from './lists.service';
+import { ListGatewayInMemory } from './gateways/list-gateway-in-memory';
+import { ListCreatedEvent } from './events/list-created.events';
 
 describe('ListsService', () => {
   let service: ListsService;
+  let listPersistenceGateway: ListGatewayInMemory;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [ListsService],
-    }).compile();
+  const eventEmitterMock = {
+    emit: jest.fn(),
+  };
 
-    service = module.get<ListsService>(ListsService);
+  beforeEach(() => {
+    listPersistenceGateway = new ListGatewayInMemory();
+    service = new ListsService(listPersistenceGateway, eventEmitterMock as any);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('Should create a list', async () => {
+    const list = await service.create({ name: 'Test List' });
+
+    expect(list.name).toBe('Test List');
+    expect(listPersistenceGateway.items).toEqual([list]);
+
+    expect(eventEmitterMock.emit).toHaveBeenCalledTimes(1);
+    expect(eventEmitterMock.emit).toHaveBeenCalledWith(
+      'list.created',
+      new ListCreatedEvent(list),
+    );
   });
 });
