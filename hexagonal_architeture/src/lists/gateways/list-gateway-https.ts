213908e2357599cc04ca6203115554dc8a750e44
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-07-11 16:33:05 -0300

    [:sparkles:] | implements: interface - gateway > http > create (lists)

--- hexagonal_architeture/src/lists/gateways/list-gateway-https.ts
+++ hexagonal_architeture/src/lists/gateways/list-gateway-https.ts
@@ -1,6 +1,8 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ListGatewayInterface } from './list-gateway-interface';
 import { HttpService } from '@nestjs/axios';
+import { List } from '../entities/list.entity';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class ListGatewayHttp implements ListGatewayInterface {
@@ -8,4 +10,13 @@ export class ListGatewayHttp implements ListGatewayInterface {
     @Inject(HttpService)
     private httpService: HttpService,
   ) {}
+
+  async create(list: List): Promise<List> {
+    await lastValueFrom(
+      this.httpService.post('list', {
+        name: list.name,
+      }),
+    );
+    return list;
+  }
 }
