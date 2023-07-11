2935e8242d917e6ee703ad79ee9679ff121d88db
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-07-11 15:57:48 -0300

    [:gear:] | setup: modules - sequelize (app)

--- hexagonal_architeture/src/app.module.ts
+++ hexagonal_architeture/src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ListsModule } from './lists/lists.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [ListsModule],
+  imports: [
+    ListsModule,
+    EventEmitterModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: 'sqlite',
+      host: ':memory:',
+      autoLoadModels: true,
+      models: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
